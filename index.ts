94e04ba717d8c864f131c3d4c7edb13961fa5cba
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-07-13 12:17:58 +0500

    changes

--- index.ts
+++ index.ts
@@ -1,13 +1,11 @@
 import { Client } from "twitter-api-sdk";
 import "dotenv/config"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-import { text } from "stream/consumers";
-
-console.log("process", process.env);
+// import { text } from "stream/consumers";
 
 const main = async () => {
   try {
     const client = new Client(process.env.BEARER_TOKEN!);
-    const { data } = await client.users.findUserByUsername("ziakhan");
+    const { data } = await client.users.findUserByUsername("shehza-d");
 
     if (!data) throw new Error("Couldn't find user");
 
@@ -16,7 +14,7 @@ const main = async () => {
     console.log("User Data: " + data);
 
   } catch (err: any) {
-    
+
     // console.log("🚀 ~ index.ts:15 ~ main ~ err:", err);
     console.log("🚀 ~ index.ts:16 ~ main ~ err:", err?.error?.detail);
   }
